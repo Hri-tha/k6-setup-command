2c11b17f1824e682ee905a96436ea485cfc7825b
Hrithik kumar Thakur <76680904+Hri-tha@users.noreply.github.com>
2024-09-20 20:41:13 +0530

    Add files via upload

---
+++ Testing_Project/k6-setup-command/Testing_Project/k6-setup-command/k6-tests/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  VUS: 10,
+  DURATION: '30s',
+  URL: 'https://hkt9137.netlify.app/',
+  METHOD: 'POST',
+  PAYLOAD: JSON.stringify({ name: 'Hrithik' }),
+  STATUS: 201,
+  SLEEP: 1
+};
